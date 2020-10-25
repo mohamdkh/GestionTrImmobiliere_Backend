642aa3da3bf43f3aa862d14bf2151b8adce02fee
HP <ftouil30@gmail.com>
2020-10-25 22:20:42 +0100

    Create Spring boot project

---
+++ GestionDesTransactionsImmobilieres/src/main/java/com/example/GestionDesTransactionsImmobilieres/GestionDesTransactionsImmobilieresApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionDesTransactionsImmobilieres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesTransactionsImmobilieresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesTransactionsImmobilieresApplication.class, args);
+	}
+
+}
---
+++ GestionDesTransactionsImmobilieres/src/test/java/com/example/GestionDesTransactionsImmobilieres/GestionDesTransactionsImmobilieresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionDesTransactionsImmobilieres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesTransactionsImmobilieresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
